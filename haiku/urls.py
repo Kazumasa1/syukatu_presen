23d2fc62cf3355567ba955ff48513889ece000b3
Kazumasa1 <kazumasa4study@gmail.com>
2021-11-15 19:56:07 +0900

    お問合せへのリンクを修正

--- haiku/urls.py
+++ haiku/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'haiku'
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
-    # path('inquiry/', views.InquiryView.as_view(), name="inquiry"),
-    path('inquiry.html/', views.InquiryView.as_view(), name="inquiry"),
+    path('inquiry/', views.InquiryView.as_view(), name="inquiry"),
+    # path('inquiry.html/', views.InquiryView.as_view(), name="inquiry"),
 ]
