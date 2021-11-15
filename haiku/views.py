dddee8594b486f967e1d8bbeb09b6f497e5e2048
Kazumasa1 <kazumasa4study@gmail.com>
2021-11-15 18:52:17 +0900

    お問合せページまでのリンクを作成

--- haiku/views.py
+++ haiku/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from . forms import InquiryForm
 # from django.shortcuts import render
 
 # Create your views here.
@@ -6,3 +7,7 @@ from django.views import generic
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
+class InquiryView(generic.FormView):
+    template_name = "inquiry.html"
+    form_class = InquiryForm
