46f0bc2a605ecd39b7ec273ee333cdd737f2c296
Kazumasa1 <kazumasa4study@gmail.com>
2021-11-16 16:16:38 +0900

    メール送信処理を記述

--- haiku/views.py
+++ haiku/views.py
@@ -1,9 +1,12 @@
+import logging
+
 from django.views import generic
-from . forms import InquiryForm
+from .forms import InquiryForm
 # from django.shortcuts import render
 
 # Create your views here.
 
+logger = logging.getLogger(__name__)
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
@@ -11,3 +14,9 @@ class IndexView(generic.TemplateView):
 class InquiryView(generic.FormView):
     template_name = "inquiry.html"
     form_class = InquiryForm
+    success_url = reverse_lazy('haiku:inquiry')
+
+    def form_valid(self, form):
+        form.send_email()
+        logger.info('inquiry sent by {}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)
