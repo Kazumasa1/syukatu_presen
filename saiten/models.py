b10bed9b99c148727cc3e9d11152135e0dfc68f5
Kazumasa1 <kazumasa4study@gmail.com>
2022-02-11 21:56:57 +0900

    スクレイピングした入賞作品を格納するモデルを作成しマイグレーション

--- saiten/models.py
+++ saiten/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Winning_senryu(models.Model):
+
+    """入賞作品モデル"""
+
+    winning_senryu = models.TextField(verbose_name='入賞作品', blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = 'Winning_senryu'
+
+    def __str__(self):
+        return self.winning_senryu
